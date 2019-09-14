a501af17cadc3d03f00d900acd0a6822934893bb
Billy Joseph <ingbillyjoseph@gmail.com>
2019-09-14 19:18:37 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 // Code your solution in this file!
+<<<<<<< HEAD
 function distanceFromHqInBlocks (blockNumber) {
     if (blockNumber > 42) {
       return blockNumber - 42;
@@ -12,6 +13,22 @@ function distanceFromHqInBlocks (blockNumber) {
   }
   
   function distanceTravelledInFeet (start, destination) {
+=======
+function distanceFromHqInBlocks(blokNumber)
+{
+  if(blokNumber > 42)
+  {
+    return blokNumber - 42;
+  }else
+  {
+    42 - blokNumber;
+  }
+}
+  function distanceFromHqInFeet (blockNumber) {
+    return distanceFromHqInBlocks(blockNumber) * 264;
+  }
+   function distanceTravelledInFeet (start, destination) {
+>>>>>>> 258ab06fce08d7fa303c6fb8dd3bde01d2908062
     if (start < destination) {
       return (destination - start) * 264;
     } else {
